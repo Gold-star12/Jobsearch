141205b27359c8815fed4c42e89e41c3e4d5935e
Gold-star12 <65475771+Gold-star12@users.noreply.github.com>
2025-06-27 07:37:41 +0530

    Add files via upload

---
+++ Data/Candidate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchDatabase.Data
+{
+    public class Candidate
+    {
+        public int CandidateId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateOnly DateOfBirth { get; set; }
+            public string Gender { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Country { get; set; }
+            public string ZipCode { get; set; }
+            public string ResumeFilePath { get; set; }
+            public string ProfileSummary { get; set; }
+
+            public virtual ICollection<Education> Educations { get; set; }
+            public virtual ICollection<WorkExperience> WorkExperiences { get; set; }
+            public virtual ICollection<JobSeekerSkill> Skills { get; set; }
+            public virtual ICollection<JobApplication> Applications { get; set; }
+        
+    }
+}
---
+++ Data/CandidateSkills.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchDatabase.Data
+{
+    public class CandidateSkill
+    {
+        public int JobSeekerId { get; set; }
+        public int SkillId { get; set; }
+        public string SkillLevel { get; set; } // "Beginner", "Intermediate", "Expert"
+        public bool IsPrimary { get; set; }
+
+        public virtual JobSeeker JobSeeker { get; set; }
+        public virtual Skill Skill { get; set; }
+    }
+}
